255225d9d942436134282367146b4c4624c50162
michael <michael.lcw99@gmail.com>
2021-12-18 18:00:18 +0800

    created fn to display time

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,50 +1,70 @@
 import { getUnit } from './unit'
-import { roundTemp, capitalizeEveryStart, capitalizeStart } from './helper'
+import { roundTemp, capitalizeEveryStart, capitalizeStart, convertFromUTC } from './helper'
 
 function displayInfo(weatherAndLoc) {
     console.log(weatherAndLoc.data); 
     const location = weatherAndLoc.location;
     const currentData = weatherAndLoc.data.current;
-    displayCurrent(currentData, location);
+    const utcOffset = weatherAndLoc.data.timezone_offset;
+    displayCurrent(currentData, utcOffset, location);
 }
 
 let displaying = null;
-function displayCurrent(data, location) {
+function displayCurrent(data, utcOffset, location) {
     const unit = getUnit();
     const infoContainer = document.querySelector(".info"); 
     let weatherUl;
+    let time;
     let locationText;
+    let weatherImg;
     let weatherDesc;
     let temperatureText;
 
     if (displaying == null) {
         weatherUl = document.createElement("ul");
         weatherUl.id = "weather-ul";
+        time = document.createElement("li");
+        time.id = "time-li";
         locationText = document.createElement("li");
         locationText.id = "locationText-li";
+        weatherImg = document.createElement("img");
+        weatherImg.id = "weather-img";
         weatherDesc = document.createElement("li");
         weatherDesc.id = "weatherDesc-li";
         temperatureText = document.createElement("li");
         temperatureText.id = "temperatureText-li";
     } else {
         weatherUl = document.querySelector("#weather-ul");
+        time = document.querySelector("#time-li");
         locationText = document.querySelector("#locationText-li");
+        weatherImg = document.querySelector("#weather-img");
         weatherDesc = document.querySelector("#weatherDesc-li");
         temperatureText = document.querySelector("#temperatureText-li");
     }
 
+    time.textContent = convertFromUTC(utcOffset);
     locationText.textContent = capitalizeEveryStart(location);
     displaying = location;
+    weatherImg = displayWeatherImg(utcOffset, data.weather[0].description);
     weatherDesc.textContent = capitalizeStart(data.weather[0].description);
     temperatureText.textContent = (unit == "metric") ? roundTemp(data.temp) + "°C" : roundTemp(data.temp) + "°F";
+    weatherUl.appendChild(time);
     weatherUl.appendChild(locationText);
     weatherUl.appendChild(weatherDesc);
     weatherUl.appendChild(temperatureText);
     infoContainer.appendChild(weatherUl);
 }
 
+// check for day/night & description
+function displayWeatherImg(utcOffset, description) {
+    switch (description) {
+        
+    }
+}
+
 function changeUnitDisplay() {
 
 }
+
 
 export default displayInfo
--- src/modules/helper.js
+++ src/modules/helper.js
@@ -16,4 +16,39 @@ function capitalizeStart(sentence) {
     return sentence.charAt(0).toUpperCase() + sentence.slice(1);
 }
 
-export { roundTemp, capitalizeEveryStart, capitalizeStart }
+// converts from UTC time to local time
+function convertFromUTC(utcOffset) {
+    const utcTime = Date.now();
+    const localTime = utcTime + utcOffset * 1000; 
+    const localTimeStr = new Date(localTime).toUTCString();
+    const result = cleanString(localTimeStr);
+    return result;
+}
+
+// pre-cond: takes in UTCString 
+// post-cond: returns in e.g Sat 18 Dec 2021 5:55pm
+function cleanString(str) {
+    let result;
+    result = str.replace("GMT", '');
+    result = result.replace(',', '');
+    const arr = result.split(" ");
+    const time = arr[4];
+    const formattedTime = convertTo12h(time);
+    arr[4] = formattedTime;
+    result = arr.join(" ");
+    return result;
+}
+
+// pre-cond: takes in HH:MM:SS format 
+// post-cond: returns 12h format
+function convertTo12h(time) {
+    const arr = time.split(":");
+    const hour = (arr[0] != '12') ? arr[0] % 12 : 12;
+    const min = arr[1];
+    const signature = (arr[0] >= 12) ? "pm" : "am";
+    const result = hour + ":" + min + signature;
+    return result;
+}
+
+
+export { roundTemp, capitalizeEveryStart, capitalizeStart, convertFromUTC }
